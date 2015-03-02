e2c2f6bc3be7ee04432ca99c950d5c1031a251de
Nate Morse <morse.nate@gmail.com>
2015-03-02 12:18:32 -0500

    Line up with graphlet

--- grapel.js
+++ grapel.js
@@ -1,56 +1,63 @@
 // grapel.js
-// graph structure (format)
-//    v0.1_min
-// {nodes:[{"name":"n0"}, {"name":"n1"}], edges:[[0, 1, "e1"]]}
-//    v0.1_expanded
-// {nodes:[{"name":"n0"}, {"name":"n1"}], edges:[["n0", "n1", "e1"]]}
-//    v0.2_expanded
-// {nodes:{"n0":{}, "n1":{}}, edges:[["n0", "n1", "e1"]]}
-// usage:
-// grapel.load({nodes:[{"name":"n0"}, {"name":"n1"}], edges:[[0, 1, "e1"]]});
-// grapel.lookup_node({"edge_index":0, "direction":"from"});
-//       --> {"name":"a"}
-// grapel.lookup_edges({"node_index":0, "direction":"out"});
-//       --> [[0, 1, "e1"]]
+// chainable graph query (gq)
+// Usage: select_nodes = gq.using(some_graph).find({node:{property:"value"}}).nodes();
+
 (function($) {
-    
-    grapel = {
-        load:function(cg) {
-            this.g = cg;
-            this.node_ref_by_type = 'unknown';
-            this.node_container_type = 'unknown';
-            // Type checking on the graph being loaded...
-            if (cg.edges && cg.edges[0] && cg.edges[0][0] && cg.nodes) {
-                if (typeof cg.edges[0][0] === 'number' && typeof cg.nodes === 'array') {
-                    this.node_ref_type = 'number';
-                    this.node_container_type = 'array';
-                } 
-                if (typeof cg.edges[0][0] === 'string' && typeof cg.nodes === 'object') {
-                    this.node_ref_type = 'string';
-                    this.node_container_type = 'object';
-                } 
-            }
-        },
-        lookup_node:function(args) {
-            var edge_attr_index = args.direction === 'from'? 0: args.direction === 'to'? 1: false;
-            var edge = this.g.edges[args.edge_index];
-            return this.g.nodes[edge[edge_attr_index]];
-        },
-        lookup_edges:function(args) {
-            var edge_index = args.direction === 'in'? 1: args.direction === 'out'? 0: false;
-            // A node index is either an integer (index) or a string (a node name), so normalize it as an index.
-            var node_index = (this.node_ref_type === 'string')? get_node_by_name(args.node_index): args.node_index;
-            var result = [];
-            $.each(this.g.edges, function(i, edge) {
-                if(edge[edge_index] === args.node_index) {
-                    result.push(edge);
-                }
-            });
-            return result;
-        }
-    };
-    var get_node_by_name = function(node_name)  {
-        _
-    }
-    
-})(jQuery);
+  var grapel = {};
+  gq = {
+		"using": function(g) {
+			grapel = g;
+			return this;
+		},
+		"find": function(sel) {
+			var res = {};
+			if (!grapel) {
+			  console.log('qg.find has an undefined grapel.');
+			  return res;
+			}
+			if (sel.element === "node") {
+				res.nodes = [];
+				$.each(grapel.nodes, function(i, n) {
+					if (sel.id && sel.id === n.id) {
+						res.nodes.push(n);
+						return false;
+					}
+					if (sel.type && n[sel.type]) {
+						res.nodes.push(n);
+					}
+				});
+			}
+			else if (sel.element === "edge") {
+				res.edges = [];
+				$.each(grapel.edges, function(i, e) {
+					if ((!sel.type || sel.type === 'all' || e.edge_type === sel.type) &&
+						(!sel.from || e.from === sel.from) &&
+						(!sel.to || e.to === sel.to)
+					) {
+						res.edges.push(e);
+					}
+				});
+			}
+			grapel = res;
+			return this;
+		},
+		"edges": function() {
+			return grapel.edges;
+		},
+		"nodes": function() {
+			return grapel.nodes;
+		},
+		"graph": function() {
+			return grapel;
+		},
+		"node_index": function(id) {
+		  var node_index = -1;
+			$.each(grapel.nodes, function(i, n) {
+				if (id === n.id) {
+					node_index = i;
+				}
+			});
+			return node_index;
+		}
+	};
+})(JQuery);
